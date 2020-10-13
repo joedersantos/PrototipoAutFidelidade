d41d9029c69e3d8bda8080cdc5d23c05c7b154bf
Joéder Marques dos Santos <jhol.si@gmail.com>
2020-10-13 20:42:48 -0300

    teste

--- src/03 - Domain/Prototipo.Domain/Handlers/SalvarEnderecoHandler.cs
+++ src/03 - Domain/Prototipo.Domain/Handlers/SalvarEnderecoHandler.cs
@@ -39,7 +39,6 @@ namespace Prototipo.Domain.Handlers
                     UF = request.UF,
                     DataCriacao = DateTime.UtcNow,
                     DataAtaulizacao = DateTime.UtcNow
-
                 };
 
                 return await repository.Inserir(endereco);
